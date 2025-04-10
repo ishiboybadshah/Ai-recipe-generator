1ca24495bd8c1b07c7120379efbe94a022766bda
Aditya patil <aditya636128@gmail.com>
2025-04-10 13:49:08 +0530

    Update utils.py

--- recipegenerator/utils.py
+++ recipegenerator/utils.py
@@ -1,11 +1,11 @@
 import openai
 import os
-from decouple import config  # make sure python-decouple is installed
+from decouple import config  
 
-# Set DeepInfra as the base API
+
 openai.api_base = "https://api.deepinfra.com/v1/openai"
 openai.api_key = config("DEEPINFRA_API_KEY")
-  # securely load from .env
+ 
 
 def generate_recipe(ingredients):
     prompt = f"Create a unique recipe using: {ingredients}"
